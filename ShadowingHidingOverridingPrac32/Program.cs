d08567ea766354faf80c3dc7a9d98371c55a33a0
Jimin Baek <qorwlalsjimin@naver.com>
2023-06-01 10:12:41 +0900

    override 예제

--- ShadowingHidingOverridingPrac32/Program.cs
+++ ShadowingHidingOverridingPrac32/Program.cs
@@ -7,22 +7,65 @@ using System.Threading.Tasks;
 
 namespace ShadowingHidingOverridingPrac32
 {
-    /// <summary>
-    /// Shadowing 예제
-    /// </summary>
     internal class Program
     {
+        //Hiding 예제
+        class Parent
+        {
+            public int Variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("어흥");
+            }
+            public virtual void Method2() //오버라이딩
+            {
+                Console.WriteLine("어흥흥");
+            }
+        }
 
-        //전역변수
+        class Child : Parent 
+        {
+            public new string Variable = "이칠삼"; //하이딩 명시 키워드 new
+            public new void Method()
+            {
+                Console.WriteLine("야옹");
+            }
+
+            public override void Method2() //오버라이딩
+            {
+                Console.WriteLine("야옹야옹");
+            }
+        }
+
+
+
+
+        //Shadowing 예제
         public static int number = 7; //같은 이름의 지역변수에 의해 가려짐(클래스 변수)
         public int number2 = 2; //같은 이름의 지역변수에 의해 가려짐(인스턴스 변수)
 
         static void Main(string[] args) //클래스 메서드가 아니라면 전역변수에 접근 가능
         {
+            /*섀도잉*/
             int number = 10;
             Console.WriteLine(number); //지역변수 number 출력
             Console.WriteLine(Program.number); //전역변수
-}
+
+            /*하이딩*/
+            Child child = new Child();
+            Console.WriteLine(child.Variable); //이칠삼
+
+            Parent p = child;
+            Console.WriteLine(p.Variable); //273
+
+            child.Method(); //야옹
+            p.Method(); //어흥
+
+            child.Method2(); //야옹
+            p.Method2(); //어흥
+
+            
+        }
 
         void doSomething()
         {
